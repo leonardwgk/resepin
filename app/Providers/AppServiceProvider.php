8887c9c4374aa7ca852c5a73cc95602c35a1b36c
chukynya <henry.christian7557@gmail.com>
2026-01-22 08:53:46 +0700

    Configuze Dockerimage & CI/CD Pipeline to Azure
    Several setting has been changed, this adjmnt is created so make the HTTP request more secure on azure cloud

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This tells Laravel: "I am behind a proxy, always use HTTPS for links and forms"
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
--- config/database.php
+++ config/database.php
@@ -58,9 +58,15 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+
+            /*
+            ** Kalau jalanin di local uncomment ini 
+            **
+            ** 'options' => []
+            */
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => base_path('certs/DigiCertGlobalRootG2.crt.pem'),
+            ],
         ],
 
         'mariadb' => [
